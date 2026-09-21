356f883944acd0e4c0c5b96d5f4bfbe7e008e431
Majestic-dev <saararonmathias1@gmail.com>
2026-09-21 21:31:32 +0300

    BRUH

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Mentornaut-Rakendus",
+  base: "/Mentornaut-Rakendus/",
   plugins: [
     tailwindcss(),
     react(),
